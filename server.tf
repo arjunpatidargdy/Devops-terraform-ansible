000fd98f3723bb14f1638085ef1a09d4fd1b360f
arjunpatidargdy <48528969+arjunpatidargdy@users.noreply.github.com>
2023-07-12 14:53:53 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -13,12 +13,12 @@ region = "us-east-1"
 resource "aws_instance" "myawsserver" {
   ami = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
-  key_name = "raman-import"
+  key_name = "arjun-import"
 
   tags = {
-    Name = "Raman-DevOps-batch-server"
+    Name = "Arjun-DevOps-batch-server"
     env = "Production"
-    owner = "Raman Khanna"
+    owner = "Arjun Patidar"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
